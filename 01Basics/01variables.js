3c525de657ccf07e60f902922d1c9992bac3f319
Sakshi Singh <94746600+251708@users.noreply.github.com>
2024-10-16 05:04:40 +0000

    datatypes

---
+++ 01Basics/01variables.js
@@ -0,0 +1,22 @@
+const AccountId = 9522
+let accountemail = "sakshi@gmail.com"
+var accountpassword = "86592"
+accountcity = "Prayagraj"
+
+//AccountId = 68671
+accountemail = "shivi@gmail.com"
+accountpassword = "86518"
+accountcity = "varanasi"
+let accountoffice;
+
+//In javascript when we declare any variable bt we not assign any value to that variable then 
+// javascript take that variable as undefined so plz keep all these little things in ur mind 
+
+//console.log(AccountId);
+console.table([accountemail , accountpassword , accountcity ,accountoffice]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
---
+++ 01Basics/02datatypes.js
@@ -0,0 +1,8 @@
+//"use strict" ; // treat all JS code as newer version 
+
+// alert ("Sakshi Singh") we are using nodejs, not browser
+
+console.table([typeof null , undefined]);
+
+console.log(typeof null);
+console.log(typeof undefined);
